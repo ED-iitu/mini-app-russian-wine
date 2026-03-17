3f663b5ae8e61be996fb701366703b0790b50de8
pelivan <pelivan@kolesa.kz>
2026-03-17 15:21:03 +0500

    fix catalog

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import CatalogView from '../views/CatalogView.vue'
 
 const routes = [
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 }
